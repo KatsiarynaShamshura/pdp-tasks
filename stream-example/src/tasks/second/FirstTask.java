b1ac96b57e37dbc18fe54db89e98aa82c96a3401
Katsiaryna Shamshura <katsiaryna_shamshura@epam.com>
2019-03-03 16:54:45 +0300

    Tasks with using stream

---
+++ stream-example/src/tasks/second/FirstTask.java
@@ -0,0 +1,43 @@
+package tasks.second;
+
+import static java.util.stream.Collectors.toList;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+public class FirstTask {
+
+    public static void main(String[] args) {
+        Map<String, DayOfWeek> map = new HashMap<>();
+
+        for (int i = 0; i < 60; i++) {
+            map.put(UUID.randomUUID().toString(), LocalDate.now().plusDays(i).getDayOfWeek());
+        }
+
+        List<DayOfWeek> dayOfScrum = map.values().stream()
+            .limit(60)
+            .filter(
+                objectObjectHashMap -> objectObjectHashMap.getValue() == DayOfWeek.MONDAY.getValue() ||
+                    objectObjectHashMap.getValue() == DayOfWeek.WEDNESDAY.getValue() ||
+                    objectObjectHashMap.getValue() == DayOfWeek.FRIDAY.getValue())
+            .collect(toList());
+
+        System.out.println(dayOfScrum);
+
+        List<DayOfWeek> dayOfTea = map.values().stream()
+            .limit(60)
+            .filter(
+                objectObjectHashMap -> objectObjectHashMap.getValue() == DayOfWeek.MONDAY.getValue() ||
+                    objectObjectHashMap.getValue() == DayOfWeek.TUESDAY.getValue() ||
+                    objectObjectHashMap.getValue() == DayOfWeek.WEDNESDAY.getValue() ||
+                    objectObjectHashMap.getValue() == DayOfWeek.THURSDAY.getValue() ||
+                    objectObjectHashMap.getValue() == DayOfWeek.FRIDAY.getValue())
+            .collect(toList());
+
+        System.out.println(dayOfTea);
+    }
+}
--- stream-example/src/tasks/second/SecondTask.java
+++ stream-example/src/tasks/second/SecondTask.java
@@ -1,51 +1,16 @@
 package tasks.second;
 
-import static java.util.stream.Collectors.toList;
-
 import java.lang.reflect.Field;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.time.DayOfWeek;
-import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
-import java.util.UUID;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
 public class SecondTask {
 
     public static void main(String[] args) {
-
-        Map<String, DayOfWeek> map = new HashMap<>();
-
-        for (int i = 0; i < 60; i++) {
-            map.put(UUID.randomUUID().toString(), LocalDate.now().plusDays(i).getDayOfWeek());
-        }
-
-        List<DayOfWeek> dayOfScrum = map.values().stream()
-            .limit(60)
-            .filter(
-                objectObjectHashMap -> objectObjectHashMap.getValue() == DayOfWeek.MONDAY.getValue() ||
-                    objectObjectHashMap.getValue() == DayOfWeek.WEDNESDAY.getValue() ||
-                    objectObjectHashMap.getValue() == DayOfWeek.FRIDAY.getValue())
-            .collect(toList());
-
-        System.out.println(dayOfScrum);
-
-        List<DayOfWeek> dayOfTea = map.values().stream()
-            .limit(60)
-            .filter(
-                objectObjectHashMap -> objectObjectHashMap.getValue() == DayOfWeek.MONDAY.getValue() ||
-                    objectObjectHashMap.getValue() == DayOfWeek.TUESDAY.getValue() ||
-                    objectObjectHashMap.getValue() == DayOfWeek.WEDNESDAY.getValue() ||
-                    objectObjectHashMap.getValue() == DayOfWeek.THURSDAY.getValue() ||
-                    objectObjectHashMap.getValue() == DayOfWeek.FRIDAY.getValue())
-            .collect(toList());
-
-        System.out.println(dayOfTea);
 
         List<User> items = new ArrayList<>();
 
