7fa1093beb372dbc09015c9ffd01743e99e22ad5
Katsiaryna Shamshura <katsiaryna_shamshura@epam.com>
2019-03-03 16:54:45 +0300

    Tasks with using stream

--- stream-example/src/tasks/second/SecondTask.java
+++ stream-example/src/tasks/second/SecondTask.java
@@ -5,6 +5,7 @@ import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
@@ -20,6 +21,9 @@ public class SecondTask {
         Field[] fields = User.class.getDeclaredFields();
         Method[] methods = User.class.getMethods();
 
+        /**
+         * I save this variant, because I like it=)
+         */
         IntStream.range(0, fields.length)
             .boxed()
             .collect(Collectors.toMap(index -> fields[index].getName(),
@@ -42,21 +46,26 @@ public class SecondTask {
                 }))
             .forEach((s, method) -> System.out.println(s + " " + method));
 
-        IntStream.range(0, fields.length)
-            .boxed()
-            .peek(index -> fields[index].setAccessible(true))
-            .collect(Collectors.toMap(index -> fields[index].getName(),
-                index -> {
-                    List<String> val = new ArrayList<>();
-                    for (User user : items) {
-                        try {
-                            val.add((String) fields[index].get(user));
-                        } catch (IllegalAccessException e) {
-                            e.printStackTrace();
-                        }
-                    }
-                    return val;
-                }))
-            .forEach((s, method) -> System.out.println(s + " " + method));
+        /**
+         * Your task
+         */
+        List<Map<String, String>> newList = new ArrayList<>();
+        for (User user : items) {
+            newList.add(IntStream.range(0, fields.length)
+                .boxed()
+                .peek(index -> fields[index].setAccessible(true))
+                .collect(Collectors.toMap(index -> fields[index].getName(),
+                    index -> getValue(user, fields[index]))));
+        }
+        System.out.println(newList);
+    }
+
+    private static String getValue(User user, Field field) {
+        try {
+            return (String) field.get(user);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+            return "default";
+        }
     }
 }
